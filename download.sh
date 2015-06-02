44a8e914b30682c234070b9f1826fe8bc1b5eb3a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2015-06-02 11:29:19 -0700

    Moved uberon downloading to a dedicated script and also download the basic obo for safe propagation.

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget --timestamping --directory-prefix download/ http://purl.obolibrary.org/obo/uberon/ext.obo
+wget --timestamping --directory-prefix download/ http://purl.obolibrary.org/obo/uberon/basic.obo
