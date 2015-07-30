86a9b754871e5ce7d91d2ef15bcc8f6a0ef6cda1
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2015-07-29 18:40:40 -0700

    Created a hetio-slim term subset
    A new notebook balled process.ipy is now the main notebook
    that downloads the obo, extracts useful information, and
    creates hetio-slim (the subset of terms we plan to use for
    our network).
    
    Currently, we do not recalculate human constraints based on
    the current obo file. Therefore, these constraints may become
    outdated.

--- download.sh
+++
@@ -1,2 +0,0 @@
-wget --timestamping --directory-prefix download/ http://purl.obolibrary.org/obo/uberon/ext.obo
-wget --timestamping --directory-prefix download/ http://purl.obolibrary.org/obo/uberon/basic.obo
